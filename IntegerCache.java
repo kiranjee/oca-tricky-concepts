fc2a1fb9d6ff084f2f58d90c0bd63dd1f389dbf0
kiranjee <32060108+kiranjee@users.noreply.github.com>
2018-11-21 14:44:10 +0530

    Commit version one

---
+++ IntegerCache.java
@@ -0,0 +1,14 @@
+public class IntegerCache {
+    public static void main(String args[]) throws Exception{
+        
+        Integer x1 = 127;
+        Integer y1 = 127;
+        
+        System.out.println(x1==y1);
+        
+        Integer x2 = 129;
+        Integer y2 = 129;
+        
+        System.out.println(x2==y2);
+    }
+}
