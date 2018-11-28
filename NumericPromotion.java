8f15afcd7830bcd557e811aad886b428da4fa2c3
kiranjee <32060108+kiranjee@users.noreply.github.com>
2018-11-28 13:07:46 +0530

    Created version one

---
+++ NumericPromotion.java
@@ -0,0 +1,12 @@
+public class NumericPromotion{
+
+     public static void main(String []args){
+        
+        byte b=10;
+        //switch(b){
+        switch(b+1){
+            case 10:
+                System.out.println("Hello");
+        }
+     }
+}
