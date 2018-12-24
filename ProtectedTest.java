bce0196ae2f923f72c01969100725380140488fb
kiranjee <32060108+kiranjee@users.noreply.github.com>
2018-12-24 12:33:16 +0530

    Created first version

---
+++ ProtectedTest.java
@@ -0,0 +1,14 @@
+import com.example.Test;
+
+class ProtectedTest extends Test{
+  public static void main(String[] args) {
+    
+    
+    //System.out.println(new Test().z);
+    System.out.println(new ProtectedTest().z);
+    //System.out.println(z);
+    
+      
+    
+  }
+}
