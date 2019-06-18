d5ca47119af5c3af77c5c9c2c3cd4c1f5fcb02df
kiranjee <32060108+kiranjee@users.noreply.github.com>
2019-06-18 13:47:33 +0530

    first commit

---
+++ ArrayStore.java
@@ -0,0 +1,11 @@
+class Base {}
+class DeriOne extends Base {}
+class DeriTwo extends Base {}
+class ArrayStore {
+  public static void main(String []args) {
+    Base [] baseArr = new DeriOne[3];
+    baseArr[0] = new DeriOne();
+    baseArr[2] = new DeriTwo();
+    System.out.println(baseArr.length);
+  }
+}
